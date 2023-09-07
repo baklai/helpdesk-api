6616a7f0d013bc273d4b643a0662db6ea922f769
Dmitrii <d.n.baklai@gmail.com>
2023-09-08 01:12:54 +0300

    fix

--- src/systools/systools.controller.ts
+++ src/systools/systools.controller.ts
@@ -11,28 +11,6 @@ import { QueryDto } from './dto/query-systool.dto';
 export class SystoolsController {
   constructor(private readonly systoolsService: SystoolsService) {}
 
-  @Get('/rdp')
-  @Header('content-type', 'application/octet-stream')
-  @ApiOperation({ summary: 'Generate an RDP (Remote Desktop Protocol) link' })
-  onLinkRDP(@Query() query: QueryDto, @Res() res: Response) {
-    const blob = this.systoolsService.linkRDP(query);
-    res.send(blob);
-  }
-
-  @Get('/vnc')
-  @ApiOperation({ summary: 'Generate a VNC (Virtual Network Computing) link' })
-  onLinkVNC(@Query() query: QueryDto, @Res() res: Response) {
-    const blob = this.systoolsService.linkVNC(query);
-    res.send(blob);
-  }
-
-  @Get('/ping')
-  @ApiOperation({ summary: 'Generate a ping link' })
-  onLinkPing(@Query() query: QueryDto, @Res() res: Response) {
-    const blob = this.systoolsService.linkPing(query);
-    res.send(blob);
-  }
-
   @Get('/ping-online')
   @ApiOperation({ summary: 'Execute a ping command and return the response' })
   onCommandPing(@Query() query: QueryDto): Promise<pingResponse> {
@@ -45,6 +23,28 @@ export class SystoolsController {
     const protocol = request.protocol;
     const host = request.get('Host');
     const blob = this.systoolsService.scriptInspector(protocol, host);
+    res.send(blob);
+  }
+
+  @Get('/ping')
+  @ApiOperation({ summary: 'Generate a ping link' })
+  onLinkPing(@Query() query: QueryDto, @Res() res: Response) {
+    const blob = this.systoolsService.linkPing(query);
+    res.send(blob);
+  }
+
+  @Get('/rdp')
+  @Header('content-type', 'application/octet-stream')
+  @ApiOperation({ summary: 'Generate an RDP (Remote Desktop Protocol) link' })
+  onLinkRDP(@Query() query: QueryDto, @Res() res: Response) {
+    const blob = this.systoolsService.linkRDP(query);
+    res.send(blob);
+  }
+
+  @Get('/vnc')
+  @ApiOperation({ summary: 'Generate a VNC (Virtual Network Computing) link' })
+  onLinkVNC(@Query() query: QueryDto, @Res() res: Response) {
+    const blob = this.systoolsService.linkVNC(query);
     res.send(blob);
   }
 }
