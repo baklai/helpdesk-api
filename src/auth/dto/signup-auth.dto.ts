f0dab6738b8fc0512d43fda2d476e890f5857f91
Dmitrii <d.n.baklai@gmail.com>
2023-10-11 17:54:51 +0300

    fix

--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -11,6 +11,8 @@ import {
 export class SignupAuthDto {
   @ApiProperty({ description: 'The login of the user', example: 'helpdesk' })
   @IsString()
+  @IsDefined()
+  @IsNotEmpty()
   readonly login: string;
 
   @ApiProperty({
@@ -18,20 +20,28 @@ export class SignupAuthDto {
     example: 'helpdesk'
   })
   @IsString()
+  @IsDefined()
+  @IsNotEmpty()
   @MinLength(6, { message: 'Password must be at least 6 characters' })
   readonly password: string;
 
   @ApiProperty({ description: 'The full name of the user', example: 'John Doe' })
   @IsString()
+  @IsDefined()
+  @IsNotEmpty()
   readonly fullname: string;
 
   @ApiProperty({ description: 'The email of the user', example: 'john@helpdesk.io' })
   @IsEmail()
   @IsString()
+  @IsDefined()
+  @IsNotEmpty()
   readonly email: string;
 
   @ApiProperty({ description: 'The phone number of the user', example: '+38(123)456-78-90' })
   @IsString()
+  @IsDefined()
+  @IsNotEmpty()
   @IsPhoneNumber()
   readonly phone: string;
 }
